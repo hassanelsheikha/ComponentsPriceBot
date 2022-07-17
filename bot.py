b6f11d8f99c53dfd6b7870209438303ff1fd7f02
Hassan El-Sheikha <71786895+hassanelsheikha@users.noreply.github.com>
2022-07-17 17:32:17 -0400

    complete bot application

--- bot.py
+++ bot.py
@@ -1,4 +1,8 @@
+import time
+
+import apscheduler.schedulers.blocking
 import discord
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord.ext import commands, tasks
 
 from store import BestBuy
@@ -6,19 +10,32 @@ from store import BestBuy
 from dotenv import load_dotenv
 import os
 
+import schedule, time
+
 load_dotenv()
 
 client = commands.Bot(command_prefix='.')
 
-mailing_list = {}
+AWAY = False
+
+MAILING_LIST = {}
 
 @client.event
 async def on_ready():
     print('Bot is running.')
+    scheduler.start()
+    print('all done')
 
+
+async def away_message(ctx):
+    await ctx.send('I am currently undergoing maintenance. Please try again later. ')
 
 @client.command()
-async def priceof(ctx, *, name):
+async def price(ctx, *, name):
+    global AWAY
+    if AWAY:
+        await away_message(ctx)
+        return
     price = BestBuy.item_price(name)
     discount = BestBuy.item_discount(name)
     url = BestBuy.item_link(name)
@@ -33,6 +50,10 @@ async def priceof(ctx, *, name):
 
 @client.command()
 async def instock(ctx, *, name):
+    global AWAY
+    if AWAY:
+        await away_message(ctx)
+        return
     stock = BestBuy.item_stock(name)
     url = BestBuy.item_link(name)
     if stock is not None:
@@ -46,25 +67,33 @@ async def instock(ctx, *, name):
 
 @client.command()
 async def notify(ctx, *, name=None):
-    print(type(ctx))
-    print(ctx.message.author)
+    global AWAY
+    if AWAY:
+        await away_message(ctx)
+        return
     if BestBuy.item_price(name) is None:
         await ctx.send(f'{name} is not sold at BestBuy. ')
     else:
 
-        mailing_list.setdefault(BestBuy.actual_name(name), [])
-        if ctx.message.author not in mailing_list[BestBuy.actual_name(name)]:
+        MAILING_LIST.setdefault(BestBuy.actual_name(name), [])
+        if ctx.message.author not in MAILING_LIST[BestBuy.actual_name(name)]:
             await ctx.send(f'Okay! I will notify you every day at 8:00am (EDT) about the details of {BestBuy.actual_name(name)} at BestBuy.')
-            mailing_list[BestBuy.actual_name(name)].append(ctx.message.author)
+            MAILING_LIST[BestBuy.actual_name(name)].append(ctx.message.author)
         else:
             await ctx.send(f'Oops! Looks like you are already on the notification list for the details of {BestBuy.actual_name(name)} at BestBuy.')
 
-        await text_mailing_list()
+
+@client.command()
+async def p(ctx):
+    await ctx.send('Hi! I am a bot that fetches prices of PC components from BestBuy. I can notify you everyday about '
+                   'the details of one or more items\n If you would like me to notify you, simply type  `.notify '
+                   'PRODUCTNAME`. If you would like the price of a product now, type `.price PRODUCTNAME`.')
 
 
-@tasks.loop(hours=24)
 async def text_mailing_list():
-    for name in mailing_list:
+    for name in MAILING_LIST:
+        if MAILING_LIST[name] == []:
+            continue
         price = BestBuy.item_price(name)
         discount = BestBuy.item_discount(name)
         stock = BestBuy.item_stock(name)
@@ -82,20 +111,35 @@ async def text_mailing_list():
 
         message += f'\nSee {url}'
 
-        for user in mailing_list[name]:
+        for user in MAILING_LIST[name]:
             await user.send(message)
 
 
-@tasks.loop(hours=24)
-async def update_mailing_list_items():
-    global mailing_list
+def update_database():
+    global AWAY
+    AWAY = True
+    print('UPDATING INVENTORY')
+    BestBuy.update_inventory()
+    print('INVENTORY DONE')
+    AWAY = False
+
+
+def update_mailing_list_items():
+    print('UPDATING MAILING LIST NOW')
+    global MAILING_LIST
     temp = {}
     for name in BestBuy.all_names():
-        if name in mailing_list:
-            temp[name] = mailing_list[name]
+        if name in MAILING_LIST:
+            temp[name] = MAILING_LIST[name]
         else:
             temp[name] = []
-    mailing_list = temp
+    MAILING_LIST = temp
+    print('DONE UPDATING')
 
 
+scheduler = AsyncIOScheduler()
+scheduler.add_job(update_database, 'cron', day_of_week='0-6', hour=1)
+scheduler.add_job(update_mailing_list_items, 'cron', day_of_week='0-6', hour=2, minute=30)
+scheduler.add_job(text_mailing_list, 'cron', day_of_week='0-6', hour=8)
 client.run(os.environ.get('bot_token'))
+
